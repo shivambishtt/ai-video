d49e93093c8e568679a97682650ba6a36cf10f1e
Shivam Bisht <rajshivam2745@gmail.com>
2025-07-08 23:58:40 +0530

    user model add

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,9 +1,25 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt"
 
-interface User{
-    name:string,
-    username:string,
-    isAdmin : boolean,
-    
+interface User {
+    email: string,
+    password: string,
+    _id?: mongoose.Types.ObjectId,
+    createdAt?: Date,
+    updatedAt?: Date,
+    name: string,
+    username: string,
+    isAdmin: boolean,
 }
+
+export const userSceham = new Schema<User>({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
