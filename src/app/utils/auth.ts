5321b5b4716ab329ad8bf63a8c0ad734d0de40fb
Shivam Bisht <rajshivam2745@gmail.com>
2025-07-13 23:12:10 +0530

    next-auth opts added

---
+++ src/app/utils/auth.ts
@@ -0,0 +1,42 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectDB from "./db";
+import User from "../models/user.model";
+import bcrypt from "bcrypt"
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                if (credentials?.email || credentials?.password) {
+                    throw new Error("Email or password is required")
+                }
+                try {
+                    await connectDB()
+                    const user = await User.findOne({ email: credentials?.email })
+                    if (!user) {
+                        throw new Error("User not found with this email")
+                    }
+
+                    const isValid = await bcrypt.compare(credentials?.password, user.password)
+                    if (!isValid) {
+                        throw new Error("Invalid password")
+                    }
+                    return {
+                        id: user._id.toString(),
+                        email: user.email
+                    }
+                } catch (error) {
+                    console.error("Auth error ", error)
+                    throw error
+                }
+            }
+        })
+
+    ]
+}
