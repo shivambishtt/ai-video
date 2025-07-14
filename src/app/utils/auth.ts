b5c993ca2a495b09d493e7648507e56cdbda4d08
Shivam Bisht <rajshivam2745@gmail.com>
2025-07-14 23:21:07 +0530

    auth file changes

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -38,5 +38,19 @@ export const authOptions: NextAuthOptions = {
             }
         })
 
-    ]
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id
+            }
+            return token
+
+        },
+        async session({ session, token }) {
+            if (session.user) {
+                session.user.id = token.id as string
+            }
+        }
+    }
 }
