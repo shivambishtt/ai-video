4b3e42373e67162cdc01c50eb17fec7d2dcbc241
Shivam Bisht <rajshivam2745@gmail.com>
2025-07-25 23:52:43 +0530

    register page added

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,7 +6,33 @@ function RegisterPage() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
-    const router= useRouter()
+    const router = useRouter()
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if (password !== confirmPassword) {
+            alert("Password does not match")
+            return
+        }
+        try {
+            const response = await fetch("/api/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                }),
+            })
+            const data = await response.json()
+            if (!response.ok) {
+                throw new Error(data.error || "Registration failed")
+            }
+        } catch (error) {
+
+        }
+    }
     return (
         <div>
             Register
